f41f1ad730137466386aff896088c52388142e9b
hitunes <hitunes247@gmail.com>
2020-02-01 12:42:10 +0200

    craete filereaderClass with dynamic filename

--- build/index.js
+++ build/index.js
@@ -1,2 +1,21 @@
 "use strict";
-console.log('hi ther');
+Object.defineProperty(exports, "__esModule", { value: true });
+var CsvFileReader_1 = require("./CsvFileReader");
+var reader = new CsvFileReader_1.CsvFileReader('football.csv');
+reader.read();
+// enum
+var MatchResult;
+(function (MatchResult) {
+    MatchResult["homeWin"] = "H";
+    MatchResult["awayWin"] = "A";
+    MatchResult["draw"] = "D";
+})(MatchResult || (MatchResult = {}));
+var manUtdWins = 0;
+for (var _i = 0, _a = reader.data; _i < _a.length; _i++) {
+    var match = _a[_i];
+    if (match[1] === 'Man United' && match[5] === MatchResult.homeWin ||
+        match[2] === 'Man United' && match[5] === MatchResult.awayWin) {
+        manUtdWins++;
+    }
+}
+console.log(manUtdWins);
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log('hi ther')
+import fs from 'fs';
+import { CsvFileReader } from './CsvFileReader';
+
+const reader = new CsvFileReader('football.csv');
+reader.read();
+// enum
+enum MatchResult {
+  homeWin ='H',
+  awayWin ='A',
+  draw ='D',
+}
+
+
+
+let manUtdWins = 0;
+
+for (let match of reader.data) {
+  if(match[1] ==='Man United' && match[5] === MatchResult.homeWin ||
+    match[2] ==='Man United' && match[5] === MatchResult.awayWin) {
+    manUtdWins ++
+  }
+}
+console.log(manUtdWins)
